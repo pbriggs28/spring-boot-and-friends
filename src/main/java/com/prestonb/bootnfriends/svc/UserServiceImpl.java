fcfad4ce4778cd5805bc83da4ca43ec7fa40c888
Preston Briggs <pbriggs28@users.noreply.github.com>
2017-10-27 10:19:30 -0700

    Added @Transactional to service layer to show exception handling.

--- src/main/java/com/prestonb/bootnfriends/svc/UserServiceImpl.java
+++ src/main/java/com/prestonb/bootnfriends/svc/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.prestonb.bootnfriends.svc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.prestonb.bootnfriends.dao.UserDao;
 import com.prestonb.bootnfriends.domain.User;
@@ -12,6 +13,7 @@ public class UserServiceImpl implements UserService {
 	@Autowired private UserDao userDao;
 
 	@Override
+	@Transactional
 	public User createUser(User newUser) {
 		return userDao.save(newUser);
 	}
